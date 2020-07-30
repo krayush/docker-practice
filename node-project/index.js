eecc16047f62375652ddb96a1902a92da114a53f
Ayush Walia <ayush.walia@phonepe.com>
2020-07-30 21:12:32 +0530

    Adding practice docker project

---
+++ node-project/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const { createConnection, client } = require("./redis");
+
+app.get("/", (_req, res) => {
+    client.get("visitorCount", (_err, reply) => {
+        const count = +(reply || 0) + 1;
+        if (count > 10) {
+            process.exit(-1);
+        }
+        client.set("visitorCount", count);
+        res.send(`HI THERE YOU GO!!! - ${count}`);
+    });
+});
+
+createConnection().then(() => {
+    app.listen(8000, () => {
+        console.log("Listening on port 8000");
+    });
+}).catch(e => {
+    console.log(e);
+});
